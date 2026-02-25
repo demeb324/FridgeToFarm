337488f4bc2f17e4e4d61cc85937977402f66cde
perla trujillo <ptrujillo79@student.cnm.edu>
2026-02-25 16:20:12 -0700

    delete home my duplicated

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,6 +1,6 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-    index("routes/home.tsx"),
+    index("routes/home/home.tsx"),
     route("/recipe", "routes/recipe/recipe.tsx"),
 ] satisfies RouteConfig;
