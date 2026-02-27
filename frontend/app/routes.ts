b370e97a8e6c1e07c7491b284fc2e32e98df5f7b
Martin Poboril <mpoboril@cnm.edu>
2026-02-27 11:55:29 -0700

    updated routes

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -3,10 +3,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
     index("routes/home/home.tsx"),
     route("/recipe", "routes/recipe/recipe.tsx"),
-<<<<<<< HEAD
-    route("/meals", "routes/meals/meals.tsx"),
-] satisfies RouteConfig;
-=======
     route('/meals','routes/meals/meals.tsx')
 ] satisfies RouteConfig
->>>>>>> c177fc6a97072b3e0750e0774dc028afa8b68bbc
