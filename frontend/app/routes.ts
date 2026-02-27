8b084a11e10b30f6d0a36b65cc3adc3ea85d8ae2
Martin Poboril <mpoboril@cnm.edu>
2026-02-27 11:50:32 -0700

    little updates on the recipe cards

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -3,4 +3,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
     index("routes/home/home.tsx"),
     route("/recipe", "routes/recipe/recipe.tsx"),
+    route("/meals", "routes/meals/meals.tsx"),
 ] satisfies RouteConfig;
