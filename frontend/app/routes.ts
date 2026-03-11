d77c41b72bec2e696b4cec11f46ee8d80735e3e5
perla trujillo <ptrujillo79@student.cnm.edu>
2026-03-11 14:02:41 -0600

    update routes

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -2,19 +2,17 @@ import {type RouteConfig, index, route, layout} from "@react-router/dev/routes";
 
 export default [
     layout('layouts/main.tsx', [
-    index("routes/home/home.tsx"),
-    route("/recipe", "routes/recipe/recipe.tsx"),
-    route('/meals','routes/meals/meals.tsx'),
-    route('/login', 'routes/login-newaccount/login.tsx'),
-    route('/friends', 'routes/friends/friends.tsx'),
-    route('/accountset', 'routes/account/accountset.tsx'),
-    route('/items-list', 'routes/items-list-page/items-list.tsx'),
-    route("/allfriends", "routes/friends/allfriends.tsx"),
-<<<<<<< HEAD
-    route('/saved-recipes', 'routes/recipe/saved-recipes.tsx'),
-=======
-    route("/friendprofile", "routes/friends/friendprofile.tsx"),
->>>>>>> friendprofile
+        index("routes/home/home.tsx"),
+        route("/recipe", "routes/recipe/recipe.tsx"),
+        route('/meals', 'routes/meals/meals.tsx'),
+        route('/login', 'routes/login-newaccount/login.tsx'),
+        route('/friends', 'routes/friends/friends.tsx'),
+        route('/accountset', 'routes/account/accountset.tsx'),
+        route('/items-list', 'routes/items-list-page/items-list.tsx'),
+        route("/allfriends", "routes/friends/allfriends.tsx"),
+        route('/saved-recipes', 'routes/recipe/saved-recipes.tsx'),
+        route("/friendprofile", "routes/friends/friendprofile.tsx"),
+
 
     ])
 ] satisfies RouteConfig
